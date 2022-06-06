ced8f707cf5fa664ce59943abcb1c7fed8a483c1
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2022-06-06 16:32:10 +0400

    Working app

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs = require('fs/promises');
+const express = require('express');
+const cors = require('cors');
+const _ = require('lodash');
+const { v4: uuid } = require('uuid');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const STORE = {
+  tops: ['Black', 'White', 'Orange', 'Navy'],
+  jeans: ['Grey', 'Dark Grey', 'Black', 'Navy'],
+  shoes: ['White', 'Grey', 'Black'],
+};
+
+app.get('/outfit', (req, res) => {
+  return res.json({
+    // _.sample return random item from collection
+    top: _.sample(STORE.tops),
+    jeans: _.sample(STORE.jeans),
+    shoes: _.sample(STORE.shoes),
+    // !todo: write own function and remove lodash
+  });
+});
+
+app.get('/comments/:id', async (req, res) => {
+  const id = req.params.id;
+  let content;
+
+  try {
+    content = await fs.readFile(`data/comments/${id}.txt`, 'utf-8');
+  } catch (err) {
+    return res.sendStatus(404);
+  }
+
+  res.json({
+    content: content,
+  });
+});
+
+app.post('/comments', async (req, res) => {
+  const id = uuid();
+  const content = req.body.content;
+
+  if (!content) {
+    return res.sendStatus(400);
+  }
+
+  await fs.mkdir('data/comments', { recursive: true });
+  await fs.writeFile(`data/comments/${id}.txt`, content);
+
+  res.status(201).json({
+    id: id,
+  });
+});
+
+app.listen(PORT, () =>
+  console.log(`API Server is running at http://localhost:${PORT}/ ...`)
+);
